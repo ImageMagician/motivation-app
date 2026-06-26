ecf11501e265c3d0fc7a2ff0f66d447146b12333
Mark Larson <mark@the-larsons.org>
2026-06-26 17:27:35 -0600

    Added tab structure and dashboard/quotes components

--- resources/js/components/admin/App.vue
+++ resources/js/components/admin/App.vue
@@ -1,10 +1,32 @@
 <script setup>
     import { ref } from 'vue';
     import Dashboard from './Dashboard.vue';
+    import QuotesManager from './QuotesManager.vue';
+
+    const tab = ref('subscribers');
 </script>
 
 <template>
+    <div class="admin">
+        <nav class="mb-8 border-b border-gray-300 pb-4">
+            <button
+                class="py-2 px-4 mx-2"
+                :class="tab === 'subscribers' ? 'font-bold' : 'font-normal'"
+                @click="tab = 'subscribers'"
+            >
+                Subscribers
+            </button>
+            <button
+                class="py-2 px-4 mx-2"
+                :class="tab === 'quotes' ? 'font-bold' : 'font-normal'"
+                @click="tab = 'quotes'">
+                Quotes
+            </button>
+        </nav>
 
+        <Dashboard v-if="tab === 'subscribers'" />
+        <QuotesManager v-else />
+    </div>
 </template>
 
 <style scoped>
