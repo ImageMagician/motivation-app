c662f947eae8260d774814b52f9b2e1a80e24d59
Mark Larson <mark@the-larsons.org>
2026-06-28 15:10:14 -0600

    App.vue: update tab styles

--- resources/js/components/admin/App.vue
+++ resources/js/components/admin/App.vue
@@ -8,17 +8,17 @@
 
 <template>
     <div class="admin">
-        <nav class="mb-8 border-b border-gray-300 pb-4">
+        <nav class="mb-8 border-b border-gray-300 px-8">
             <button
-                class="py-2 px-4 mx-2"
-                :class="tab === 'subscribers' ? 'font-bold' : 'font-normal'"
+                class="py-1 px-4 mx-1"
+                :class="tab === 'subscribers' ? 'font-bold border-l border-t border-r border-gray-300 border-b-white translate-y-0.25 bg-white rounded-t-lg' : 'font-normal'"
                 @click="tab = 'subscribers'"
             >
                 Subscribers
             </button>
             <button
-                class="py-2 px-4 mx-2"
-                :class="tab === 'quotes' ? 'font-bold' : 'font-normal'"
+                class="py-1 px-4 mx-1"
+                :class="tab === 'quotes' ? 'font-bold border-l border-t border-r border-gray-300 border-b-white translate-y-0.25 bg-white rounded-t-lg' : 'font-normal'"
                 @click="tab = 'quotes'">
                 Quotes
             </button>
